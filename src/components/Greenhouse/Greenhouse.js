4cae1188ea0517f4cab3ca01169a91edd64dfea5
Nicholas Brancifort <brancifortnick@yahoo.com>
2021-06-10 13:55:29 -0400

    path corrected

--- src/components/Greenhouse/Greenhouse.js
+++ src/components/Greenhouse/Greenhouse.js
@@ -1,19 +1,19 @@
-import dayImage from './images/greenhouse-day.jpg';
-import nightImage from './images/greenhouse-night.jpg';
+import { useTheme } from "../../context/ThemeContext";
+import dayImage from "./images/greenhouse-day.jpg";
+import nightImage from "./images/greenhouse-night.jpg";
+import "./Greenhouse.css";
 
-import './Greenhouse.css';
-
-import LightSwitch from './LightSwitch';
-import ClimateStats from './ClimateStats';
-
-function Greenhouse() {
+import LightSwitch from "./LightSwitch";
+import ClimateStats from "./ClimateStats";
+const switchTheme = useTheme === 'day' ? dayImage : nightImage;
+const dayNight = function Greenhouse() {
   return (
     <section>
-      <img className='greenhouse-img' src={dayImage} alt='greenhouse' />
+      <img className="greenhouse-img" src={dayImage} alt="greenhouse" />
       <LightSwitch />
       <ClimateStats />
     </section>
   );
-}
+};
 
-export default Greenhouse;
+export default dayNight;
