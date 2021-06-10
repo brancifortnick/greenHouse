bad7d6674c484257239e2e873ee4557aadde1ba0
Nicholas Brancifort <brancifortnick@yahoo.com>
2021-06-10 17:46:54 -0400

    phase 6 complete

--- src/context/ClimateContext.js
+++ src/context/ClimateContext.js
@@ -9,11 +9,11 @@ export const useClimate = () => useContext(ClimateContext);
 
 export default function ClimateProvider ({ children }) {
   const [temperature, setTemperature] = useState(50);
-
+  const [humidity, setHumidity] = useState(40);
   return (
     <ClimateContext.Provider
-      key={temperature}
-      value={{ temperature, setTemperature }}
+      key={(temperature, humidity)}
+      value={{ temperature, setTemperature, humidity, setHumidity }}
     >
       {children}
     </ClimateContext.Provider>
