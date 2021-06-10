f5504f0aad91f90ebb330a58b1bc5ebcfddfafb1
Machaelmus <michaeljtufo@gmail.com>
2021-06-10 15:50:21 -0400

    phase 2 lightswitch :D

--- src/components/Greenhouse/Greenhouse.js
+++ src/components/Greenhouse/Greenhouse.js
@@ -1,19 +1,23 @@
 import { useTheme } from "../../context/ThemeContext";
+
 import dayImage from "./images/greenhouse-day.jpg";
 import nightImage from "./images/greenhouse-night.jpg";
 import "./Greenhouse.css";
 
 import LightSwitch from "./LightSwitch";
 import ClimateStats from "./ClimateStats";
-const switchTheme = useTheme === 'day' ? dayImage : nightImage;
-const dayNight = function Greenhouse() {
+
+
+function Greenhouse() {
+  const {themeName} = useTheme();
+
   return (
     <section>
-      <img className="greenhouse-img" src={dayImage} alt="greenhouse" />
+      <img className="greenhouse-img" src={themeName === 'day' ? dayImage : nightImage} alt="greenhouse" />
       <LightSwitch />
       <ClimateStats />
     </section>
   );
 };
 
-export default dayNight;
+export default Greenhouse;
